06fe58edd0f9f800f2385eb5935890cb6c4f9610
ketpradh <74880366+ketpradh@users.noreply.github.com>
2021-03-07 21:54:47 -0500

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -13,14 +13,6 @@ lot_summary <- suspension_coils %>% group_by(Manufacturing_Lot) %>% summarize(Me
 t.test(log10(suspension_coils$PSI),mu=mean(log10(suspension_coils$PSI)))
 
 #Per Manufacturing Lots
-Lot1 <- subset(suspension_coils,Manufacturing_Lot=='Lot1')
-Lot2 <- subset(suspension_coils,Manufacturing_Lot=='Lot2')
-Lot3 <- subset(suspension_coils,Manufacturing_Lot=='Lot3')
-t.test(log10(Lot1$PSI),mu=mean(log10(suspension_coils$PSI)))
-t.test(log10(Lot2$PSI),mu=mean(log10(suspension_coils$PSI)))
-t.test(log10(Lot3$PSI),mu=mean(log10(suspension_coils$PSI)))
-
-#Alternatively
 t.test(log10(subset(suspension_coils,Manufacturing_Lot=='Lot1')$PSI),mu=mean(log10(suspension_coils$PSI)))
 t.test(log10(subset(suspension_coils,Manufacturing_Lot=='Lot2')$PSI),mu=mean(log10(suspension_coils$PSI)))
 t.test(log10(subset(suspension_coils,Manufacturing_Lot=='Lot3')$PSI),mu=mean(log10(suspension_coils$PSI)))
